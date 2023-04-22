185805dd50b693978dd4e4f8bf2afb150333130e
Maa-mmy <dikecynthia22@gmail.com>
2023-04-22 05:34:44 +0100

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-# creates a file in /tmp
-
+# creates a file in /tmp 
 file { '/tmp/school' :
- permission => '0744',
- owner      => 'www-data',
- group      => 'www-data',
- content    => 'I love Puppet',
+permission => '0744',
+owner      => 'www-data',
+group      => 'www-data',
+content    => 'I love Puppet',
 }
