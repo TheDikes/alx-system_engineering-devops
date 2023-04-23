675c837a818f1ecfd24a8642c5e9fe3d0835f91f
Maa-mmy <dikecynthia22@gmail.com>
2023-04-24 00:27:23 +0100

    Client configuration file

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,11 @@
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => '    IdentityFile ~/.ssh/school',
+  match => '^#?\s*IdentityFile',
+}
+
+file_line { 'Turn off passwd auth':
+  path => '/etc/ssh/ssh_config',
+  line => '    PasswordAuthentication no',
+  match => '^#?\s*PasswordAuthentication',
+}
