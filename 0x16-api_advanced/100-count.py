d1ae6019cd6814dd08d127009bb432478c890c1e
Maa-mmy <dikecynthia22@gmail.com>
2023-06-08 23:13:58 +0100

    Count

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,41 +1,45 @@
 #!/usr/bin/python3
-"""Function to count words in all hot posts of a given Reddit subreddit."""
+"""
+Queries the Reddit API, parses the title of all hot articles, and prints a
+sorted count of given keywords.
+"""
 import requests
-from collections import Counter
 
-def count_words(subreddit, word_list, after=None, counts=None):
-    if counts is None:
-        counts = Counter()
 
-    if after is None:
-        url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    else:
-        url = f"https://www.reddit.com/r/{subreddit}/hot.json?after={after}"
+def count_words(subreddit, word_list, hot_list=[], viewed_count=0, after=''):
+    """
+    Queries the Reddit API, parses the title of all hot articles, and prints a
+    sorted count of given keywords.
+    """
+    base = 'https://www.reddit.com/'
+    endpoint = 'r/{}/hot.json'.format(subreddit)
+    query_string = '?show="all"&limit=100&after={}&count={}'.format(
+        after, viewed_count)
+    url = base + endpoint + query_string
+    headers = {'User-Agent': 'Python/1.0(Holberton School 0x16 task 3)'}
+    response = requests.get(url, headers=headers)
+    if not response.ok:
+            return
 
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        return
+    data = response.json()['data']
+    for post in data['children']:
+        hot_list.append(post['data']['title'])
+    after = data['after']
+    dist = data['dist']
+    if (after):
+        count_words(subreddit, [], hot_list, viewed_count + dist, after)
 
-    data = response.json().get("data")
-    after = data.get("after")
+    if viewed_count == 0:
+        result = {}
+        word_list = [word.lower() for word in word_list]
+        hot_words = ' '.join(hot_list).lower().split(' ')
+        for hot_word in hot_words:
+            for search_word in word_list:
+                if hot_word == search_word:
+                    result.setdefault(search_word, 0)
+                    result[search_word] += 1
 
-    for child in data.get("children"):
-        title = child.get("data").get("title").lower()
-        words = title.split()
-
-        for word in word_list:
-            if word.lower() in words:
-                counts[word.lower()] += 1
-
-    if after is not None:
-        return count_words(subreddit, word_list, after, counts)
-
-    sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-    for word, count in sorted_counts:
-        print(f"{word}: {count}")
-
-# Example usage
-count_words("programming", ["python", "java", "javascript"])
+        for word, count in sorted(
+            sorted(result.items()), key=lambda x: x[1], reverse=True
+        ):
+            print("{}: {}".format(word, count))
